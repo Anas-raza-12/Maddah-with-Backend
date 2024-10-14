07c59e313703419bb40afb9e68283c1c4fc4611d
Anas-raza-12 <aranasraza123@gmail.com>
2024-10-14 12:11:35 +0500

    cart view page

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,3 +1,51 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+@extends('layouts.main') @section('title', 'Cart') @section('bodyClass', 'cart') @section('content')
+
+<div class="links container d-flex">
+    <a class="disable" href="">Home</a> /
+    <a class="Active" href="">Cart View</a>
 </div>
+
+<div class="first-container container">
+    <div class="row">
+        <div class="col-lg-3"><h3>Product</h3></div>
+        <div class="col-lg-3"><h3>Price</h3></div>
+        <div class="col-lg-3"><h3>Quantity</h3></div>
+        <div class="col-lg-3">
+            <h3>Total</h3>
+        </div>
+    </div>
+
+    <div class="row product-item position-relative">
+        <div class="col-lg-3"><img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/silhouette-of-a-guy-with-a-cap-at-red-sky-sunset-free-image.jpeg?h=800&quality=80" alt="product image" /></div>
+        <div class="col-lg-3"><p class="price">$60</p></div>
+        <div class="col-lg-3"><input type="number" placeholder="01" class="quantity" value="1" min="1" /></div>
+        <div class="col-lg-3"><p class="total-price">$60</p></div>
+        <div class="close position-absolute">Remove </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script>
+    const productItems = document.querySelectorAll(".product-item");
+
+    productItems.forEach((item) => {
+        let priceElement = item.querySelector(".price");
+        let quantityInput = item.querySelector(".quantity");
+        let totalPriceElement = item.querySelector(".total-price");
+
+        let priceText = priceElement.textContent;
+        let price = parseFloat(priceText.replace("$", ""));
+
+        quantityInput.addEventListener("input", function () {
+            let quantity = parseInt(quantityInput.value);
+            if (isNaN(quantity) || quantity < 1) {
+                quantity = 1;
+            }
+
+            let total = price * quantity;
+            totalPriceElement.textContent = ` $${total.toFixed(2)}`;
+        });
+    });
+</script>
+
+@endsection
--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -1,3 +1,64 @@
-<div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
+@extends('layouts.main') @section('title', 'Wishlist') @section('bodyClass', 'wishlist') @section('content')
+<div class="first-container container my-4">
+    <div class="d-flex wishlist">
+        <h2>Wishlist <span>(5)</span></h2>
+        <a href="">Move All To Bag</a>
+    </div>
+    <div class="wishlist-row row">
+        <div class="col-lg-2 col-md-3">
+            <div style="border-radius: 5px; overflow: hidden;" class="position-relative rounded p-0">
+                <a href="">
+                    <img
+                        width="100%"
+                        src="https://s3-alpha-sig.figma.com/img/dfd4/5c5b/3c137a48f8335aae24dfb4eafe537e01?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=no7OoqhHhJdto0F0R2-WkUP2GNG1-MmRm-cyK7TnBNtyaxE-oRwqzUYZpLOxg8uxY3uukEPZc3G2O5JigmWzF4tizCuClcpnZNV0Y~IF2peN0tiUl8klNADfP~Okg~8Rs~lPrEKfw0LMYBJKnn3p0FCmh7ri~5EuppyutL5dryB4fdDP5qzqM8yN3r7A1T9r8DvxE4v6BcS5UzhXTJ7aKvoDEjZkGiFRjBNeLuMfVmGcM3sXwNDyUb4x9hDIHb9N8pSFInuAg8ht1FYuoRS9bfJfoMIuHOp~rBu4qSxdKG3PtyTakzqOzawfz2~-BhFnsau2n4MdBqS0TJGNKrACWg__"
+                        alt=""
+                    />
+                </a>
+
+                <button  class="addCart-btn">
+                    <i class="fa-solid fa-cart-shopping"> </i>
+                    Add To Cart
+                </button>
+                <div style="position: absolute;">
+                    <p class="rounded" style="background: #db4444; color: #fff;">-35%</p>
+                    <a href="" class="wishlist-btn"><i class="fa-regular fa-heart"></i> </a>
+                </div>
+            </div>
+            <div class="tittel">Musical Instruments</div>
+            <div class="prices">$960 <span> $1106 </span></div>
+            <div class="rating"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> <span>(65)</span></div>
+        </div>
+    </div>
+    <div class="second-container container my-4">
+        <div class="d-flex wishlist">
+            <h2>Just For You</h2>
+            <a href="">See All</a>
+        </div>
+        <div class="wishlist-row row">
+            <div class="col-lg-2 col-md-3">
+                <div style="border-radius: 5px; overflow: hidden;" class="position-relative rounded p-0">
+                    <a href="">
+                        <img
+                            width="100%"
+                            src="https://s3-alpha-sig.figma.com/img/dfd4/5c5b/3c137a48f8335aae24dfb4eafe537e01?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=no7OoqhHhJdto0F0R2-WkUP2GNG1-MmRm-cyK7TnBNtyaxE-oRwqzUYZpLOxg8uxY3uukEPZc3G2O5JigmWzF4tizCuClcpnZNV0Y~IF2peN0tiUl8klNADfP~Okg~8Rs~lPrEKfw0LMYBJKnn3p0FCmh7ri~5EuppyutL5dryB4fdDP5qzqM8yN3r7A1T9r8DvxE4v6BcS5UzhXTJ7aKvoDEjZkGiFRjBNeLuMfVmGcM3sXwNDyUb4x9hDIHb9N8pSFInuAg8ht1FYuoRS9bfJfoMIuHOp~rBu4qSxdKG3PtyTakzqOzawfz2~-BhFnsau2n4MdBqS0TJGNKrACWg__"
+                            alt=""
+                        />
+                    </a>
+
+                    <button  class="addCart-btn">
+                    <i class="fa-solid fa-cart-shopping"> </i>
+                    Add To Cart
+                </button>
+                    <div style="position: absolute;">
+                        <p class="rounded" style="background: #db4444; color: #fff;">-35%</p>
+                        <a href="" class="wishlist-btn"><i class="fa-regular fa-heart"></i> </a>
+                    </div>
+                </div>
+                <div class="tittel">Musical Instruments</div>
+                <div class="prices">$960 <span> $1106 </span></div>
+                <div class="rating"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> <span>(65)</span></div>
+            </div>
+        </div>
+    </div>
+    @endsection
 </div>
